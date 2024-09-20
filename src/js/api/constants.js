189ff4ea8e96a2c9f181732df217900243a0a2ce
Leanne <lemeyer.no@gmail.com>
2024-09-20 09:31:34 +0200

    feat(auth): Added register auth API
    Added register auth api constant to constants.js file

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -8,7 +8,7 @@ export const API_AUTH = `${API_BASE}/auth`;
 
 export const API_AUTH_LOGIN = `${API_AUTH}/login`;
 
-export const API_AUTH_REGISTER = `${API_AUTH}/register`;
+export const API_AUTH_REGISTER = `https://v2.api.noroff.dev/auth/register`;
 
 export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
 
