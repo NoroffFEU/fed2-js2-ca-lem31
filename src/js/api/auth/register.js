4c32227b61a5f63c510e44b1d66c22bdb5bc0b48
Leanne <lemeyer.no@gmail.com>
2024-09-22 12:52:15 +0200

    feat: Imported constants and Added Register function
    feat: imported register constants and added Register function to register file

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -2,4 +2,26 @@ import { REG_FORM } from "../constants";
 
 import { ERROR_MESSAGE } from "../constants";
 
+import { REG_FORM_OBJECT} from "../constants";
+
+import { REG_FORM_DATA } from "../constants";
+
+import { REQUEST_BODY_REG } from "../constants";
+
+import { RESPONSE_REG } from "../constants";
+
 REG_FORM.addEventListener("submit", register);
+
+async function register(event) {
+  event.preventDefault();
+
+  REG_FORM_OBJECT;
+
+  REQUEST_BODY_REG;
+};
+
+try{
+  RESPONSE_REG;
+}
+
+ 
