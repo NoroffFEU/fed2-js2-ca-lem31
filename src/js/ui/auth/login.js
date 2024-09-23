ab14fc381f2533b7a7e2e68a1d0e6369ea0ec8ca
Leanne <lemeyer.no@gmail.com>
2024-09-23 21:59:54 +0200

    fix: onLogin function
    Fixed onLogin function in auth/login.js

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,6 +1,8 @@
 import { login } from "../../api/auth/login.js";
+import { LOGIN_FORM } from "../../api/constants.js";
 
 export async function onLogin(event) {
   event.preventDefault();
   await login(event);
+  LOGIN_FORM.addEventListener("submit", login);
 }
