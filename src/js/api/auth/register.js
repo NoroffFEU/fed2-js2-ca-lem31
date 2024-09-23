d6bb53b78fc333cc2685ce42863e541cf1e5c078
Leanne <lemeyer.no@gmail.com>
2024-09-23 18:23:49 +0200

    fix: register function
    fixed register function

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -42,33 +42,3 @@ export async function register(event) {
     console.error("Error:", error);
   }
 }
-
-// export async function register(event) {
-//   event.preventDefault();
-
-//   REG_FORM_OBJECT;
-//   REG_FORM_DATA;
-
-//   REQUEST_BODY_REG;
-
-//   try {
-//     RESPONSE_REG;
-
-//     REG_DATA;
-
-//     if (RESPONSE_REG.ok) {
-//       alert("Registration successful");
-//       console.log(REG_DATA);
-//       window.location.href = ".../login.html";
-//       ERROR_MESSAGE.textContent = "";
-//     } else if (RESPONSE_REG.status === 400) {
-//       ERROR_MESSAGE.textContent = "Error: User already exists";
-//     } else if (RESPONSE_REG.status === 409) {
-//       ERROR_MESSAGE.textContent = "Error: User already exists";
-//     } else {
-//       ERROR_MESSAGE.textContent = "Error: Something went wrong";
-//     }
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
