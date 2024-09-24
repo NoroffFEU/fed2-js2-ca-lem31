b7e058e4fd77b59d74fab874a9324705996db771
Leanne <lemeyer.no@gmail.com>
2024-09-24 19:17:59 +0200

    fix: name issue
    Fixed issue with finding name in constants.js

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -14,13 +14,13 @@ export const API_AUTH_KEY = `${API_AUTH}/create-api-key`;
 
 export const API_SOCIAL = `${API_BASE}/social`;
 
-const USER_NAME = JSON.parse(localStorage.getItem("user"));
+const USER_NAME = JSON.parse(localStorage.getItem("user")) || {};
 
-const NAME = USER_NAME.name;
+const NAME = USER_NAME.name || "defaultName";
 
 export const USER_POSTS_API = `https://v2.api.noroff.dev/social/profiles/${NAME}/posts`;
 
-export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`;
+// export const API_SOCIAL_PROFILES = `;
 
 export const ERROR_MESSAGE = document.getElementById("error-message");
 
