72ed28b385fa49db9d91a3fa5fb2b4963413c5d2
Leanne <lemeyer.no@gmail.com>
2024-09-26 18:47:13 +0200

    Fix: Modified create post code
    Modified create post code to use best practices. Added API code to api/post/create.js

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,67 @@
+import { API_KEY } from "../constants";
+import { CREATE_POST_API } from "../constants";
 
+const CREATE_POST_FORM = document.getElementById("create-form");
+
+export async function getCreatePostDataAndSendToAPI(event) {
+  event.preventDefault();
+  try {
+    const CREATE_POST_REQUEST_BODY = createRequestBody(CREATE_POST_FORM);
+    const RESPONSE = await sendRequestToAPI(CREATE_POST_REQUEST_BODY);
+    handleResponse(RESPONSE);
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+function createRequestBody(CREATE_POST_FORM) {
+  const FORM_OBJECT = new FormData(CREATE_POST_FORM);
+  const FORM_DATA = Object.fromEntries(FORM_OBJECT);
+  const TAGS_ARRAY = FORM_DATA.tags.split(",").map((tag) => tag.trim());
+  return {
+    title: FORM_DATA.title,
+    body: FORM_DATA.body,
+    tags: TAGS_ARRAY,
+    media: {
+      url: FORM_DATA.media,
+      alt: FORM_DATA.alt,
+    },
+  };
+}
+
+async function sendRequestToAPI(CREATE_POST_REQUEST_BODY) {
+  const ACCESS_TOKEN = localStorage.getItem("accessToken");
+  if (!ACCESS_TOKEN) {
+    throw new Error("No access token found. Please log in.");
+  }
+  try {
+    const RESPONSE = await fetch(CREATE_POST_API, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        "X-Noroff-API-Key": API_KEY,
+      },
+      body: JSON.stringify(CREATE_POST_REQUEST_BODY),
+    });
+
+    alert("Post created successfully");
+    window.location.href = "/profile/";
+
+    const DATA = await RESPONSE.json();
+    return DATA;
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+function handleError(error) {
+  console.error("Error:", error);
+  if (ERROR_MESSAGE) {
+    ERROR_MESSAGE.textContent = `Error: ${
+      error.message || "Something went wrong"
+    }`;
+  } else {
+    alert(`Error: ${error.message || "Something went wrong"}`);
+  }
+}
