bda5069848d18c472d22dd56d91281c29b8094d9
Leanne <lemeyer.no@gmail.com>
2024-09-26 12:23:12 +0200

    Fix: Updated post creat.js file
    Updated code in post create.js

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,14 @@
-export async function onCreatePost(event) {}
+import { getSpecifiedFormDataAndSendToAPI } from "../../api/post/update";
+
+function createPost() {
+  const createPostForm = document.getElementById("create-form");
+  if (createPostForm) {
+    createPostForm.addEventListener("submit", (event) =>
+      getSpecifiedFormDataAndSendToAPI(event, createPostForm, "create")
+    );
+  }
+}
+
+export async function onCreatePost(event) {
+  createPost(event);
+}
