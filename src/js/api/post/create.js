fc4e8fa5d704e9cbc8b0ce1561eb66b706a8a61b
Leanne <lemeyer.no@gmail.com>
2024-09-26 13:45:01 +0200

    Delete: unnecessary code
    Deleted unnecessary code in create.js

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,13 +1,1 @@
-const createPostAPI = "https://v2.api.noroff.dev/social/posts";
-import { getSpecifiedFormDataAndSendToAPI } from "./update";
 
-function createPost() {
-  const createPostForm = document.getElementById("create-form");
-  if (createPostForm) {
-    createPostForm.addEventListener("submit", (event) =>
-      getSpecifiedFormDataAndSendToAPI(event, createPostForm, "create")
-    );
-  }
-}
-
-createPost();
