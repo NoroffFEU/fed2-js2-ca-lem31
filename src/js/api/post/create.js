77184c00792687fcd1041b812043fcba70a93a7e
Leanne <lemeyer.no@gmail.com>
2024-09-26 11:51:59 +0200

    delete and feat: function
    Deleted original function from create.js
    Added new function with constants
    Imported a function

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,13 @@
-export async function createPost({ title, body, tags, media }) {}
+const createPostAPI = "https://v2.api.noroff.dev/social/posts";
+import { getSpecifiedFormDataAndSendToAPI } from "./update";
+
+function createPost() {
+  const createPostForm = document.getElementById("create-form");
+  if (createPostForm) {
+    createPostForm.addEventListener("submit", (event) =>
+      getSpecifiedFormDataAndSendToAPI(event, createPostForm, "create")
+    );
+  }
+}
+
+createPost();
