a61335cdd04a4733dca855bbc186eb32cfd6ae4b
Leanne <lemeyer.no@gmail.com>
2024-09-26 06:06:54 +0200

    fix: authGuard function
    fixed authGuard function

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,5 +1,9 @@
+//ACCESS TOKEN constants
+
+//ACCESS TOKEN constants
+
 export function authGuard() {
-  if (!localStorage.token) {
+  if (!localStorage.accessToken) {
     alert("You must be logged in to view this page");
     window.location.href = "/auth/login/";
   }
