f813946331c070fa75e7df0e7b576fb7da2f06f8
Leanne <lemeyer.no@gmail.com>
2024-09-27 14:17:01 +0200

    FIX: removed old code and added new
    Removed function and added a new function to remove acces token from local storage and relocate to the login page when logging out

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,1 +1,6 @@
-export function onLogout() {}
+export function removeTokenFromStorage() {
+  // Clear user session or token
+  localStorage.removeItem("accessToken");
+  // Redirect to login page
+  window.location.href = "/auth/login/";
+}
