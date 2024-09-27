a31686a3e77fcd6e315ac29bff45140100afc5b6
Leanne <lemeyer.no@gmail.com>
2024-09-27 22:14:05 +0200

    DELETE & FEAT
    Deleted unnecessary code
    
    Added imports for API key and getSinglePost function
    
    added retrievePostID function and called it

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,1 +1,17 @@
-alert("Single Post Page");
+// import { retrievePostID } from "../../api/post/read.js";
+import { API_KEY } from "../../api/constants.js";
+import { getSinglePost } from "../../api/post/read.js";
+
+export function retrievePostID() {
+  const PARAMS = new URLSearchParams(window.location.search);
+  const POST_ID = PARAMS.get("id");
+  console.log(POST_ID);
+
+  if (POST_ID !== null) {
+    getSinglePost(POST_ID);
+  } else {
+    console.error("No post ID found in URL");
+  }
+}
+
+retrievePostID();
