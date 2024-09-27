d550d804b1f816f05eb628709da4c9cad6641af7
Leanne <lemeyer.no@gmail.com>
2024-09-27 11:30:51 +0200

    Delete: removed function
    Removed unnecessary function from app.js

--- src/app.js
+++ src/app.js
@@ -3,21 +3,3 @@ import "./css/style.css";
 import router from "./js/router/index.js";
 
 await router(window.location.pathname);
-
-export function populateEditForm() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const postId = urlParams.get("id");
-
-  if (postId) {
-    // Fetch the post data from your data source using the postId
-    fetch(`/api/posts/${postId}`)
-      .then((response) => response.json())
-      .then((post) => {
-        // Populate the form fields with the post data
-        document.querySelector("title").value = post.title;
-        document.querySelector("body").value = post.body;
-        // Add more fields as necessary
-      })
-      .catch((error) => console.error("Error fetching post:", error));
-  }
-}
