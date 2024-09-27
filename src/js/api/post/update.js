efb56bb44c971409ea89d1588c45498dc9d30f71
Leanne <lemeyer.no@gmail.com>
2024-09-27 11:18:05 +0200

    Delete & Feat: import and function
    Removed unnecessary imports and added function to populate edit form

--- src/js/api/post/update.js
+++ src/js/api/post/update.js
@@ -1,3 +1,29 @@
 import { API_KEY } from "../constants";
-import { ERROR_MESSAGE } from "../constants";
-import { UPDATE_POST_API } from "../constants";
+
+export async function populateEditForm() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const postId = urlParams.get("id");
+
+  if (postId) {
+    try {
+      const response = await fetch(
+        `https://v2.api.noroff.dev/social/posts/${postId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            "X-Noroff-API-Key": API_KEY,
+          },
+        }
+      );
+      const post = await response.json();
+
+      document.querySelector("#title").value = post.data.title;
+      document.querySelector("#body").value = post.data.body;
+      document.querySelector("#tags").value = post.data.tags.join(", ");
+      document.querySelector("#media").value = post.data.media.url;
+      document.querySelector("#alt").value = post.data.media.alt;
+    } catch (error) {
+      console.error("Error fetching post:", error);
+    }
+  }
+}
