5ae4400ebc4c20fa125e545c104af3b785f048e6
Leanne <lemeyer.no@gmail.com>
2024-09-27 22:43:42 +0200

    DELETE: Unused code commented out

--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -1,6 +1,4 @@
 import { onLogin } from "../../ui/auth/login";
 import { LOGIN_FORM } from "../../api/constants.js";
 
-// LOGIN_FORM.addEventListener("submit", onLogin);
-
 LOGIN_FORM.addEventListener("submit", onLogin);
