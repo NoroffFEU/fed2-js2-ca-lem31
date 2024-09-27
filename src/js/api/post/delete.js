52a2e3d8d14e072e8f18eca2d75d63093921bc44
Leanne <lemeyer.no@gmail.com>
2024-09-27 14:49:59 +0200

    Fix: removed original code and added mine
    Removed original code and added my delete functions

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,1 +1,31 @@
-export async function deletePost(id) {}
+import { ACCESS_TOKEN } from "../constants";
+import { GET_POST_API } from "../constants";
+import { API_KEY } from "../constants";
+import { handleError } from "./create.js";
+
+export async function removePostFromAPI(postId) {
+  try {
+    const RESPONSE = await fetch(`${GET_POST_API}${postId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        "X-Noroff-API-Key": API_KEY,
+      },
+    });
+
+    if (RESPONSE.ok) {
+      alert("Post deleted successfully");
+      window.location.href = "/profile/";
+    } else {
+      const ERROR_DATA = await RESPONSE.json();
+      throw new Error(ERROR_DATA.message || "Failed to delete post");
+    }
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+function deletePost(event) {
+  const POST_ID = event.target.dataset.id;
+  removePostFromAPI(POST_ID);
+}
