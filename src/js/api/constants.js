15cef348e6394bee522113a81c6e43a6abe6ee68
Leanne <lemeyer.no@gmail.com>
2024-09-27 11:17:31 +0200

    Fix: Post API link
    Updated link for fetching post from API

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -12,8 +12,7 @@ export const API_AUTH_REGISTER = `https://v2.api.noroff.dev/auth/register`;
 
 export const CREATE_POST_API = "https://v2.api.noroff.dev/social/posts";
 
-export const UPDATE_POST_API = (POST_ID) =>
-  `https://v2.api.noroff.dev/social/posts/${POST_ID}`;
+export const GET_POST_API = `https://v2.api.noroff.dev/social/posts/`;
 
 export const API_SOCIAL = `${API_BASE}/social`;
 
