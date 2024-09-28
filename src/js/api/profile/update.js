a6bbad76f536625dcafebef0be2d6d0659e6ea5e
Leanne <lemeyer.no@gmail.com>
2024-09-28 14:30:54 +0200

    FIX: constants
    Moved constants to constants.js and imported them instead

--- src/js/api/profile/update.js
+++ src/js/api/profile/update.js
@@ -1,7 +1,8 @@
-const UPDATE_PROFILE_BUTTON = document.getElementById("update-profile-button");
-const PROFILE_FORM = document.getElementById("Update-Profile");
-const CANCEL_BUTTON = document.getElementById("cancel-button");
-PROFILE_FORM.style.display = "none";
+import {
+  UPDATE_PROFILE_BUTTON,
+  PROFILE_FORM,
+  CANCEL_BUTTON,
+} from "../constants.js";
 
 export function closeUpdateProfileForm() {
   if (PROFILE_FORM && PROFILE_FORM.style.display === "block") {
