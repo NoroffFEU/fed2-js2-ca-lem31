eddea71a87a8811d444aefbae6e95d0c96c9983d
Leanne <lemeyer.no@gmail.com>
2024-09-28 18:40:52 +0200

    FIX & FEAT: created function & added JS DOCS
    Created function and moved the event listener into the function and called it
    
    Added JS DOCS for the function

--- src/js/router/views/login.js
+++ src/js/router/views/login.js
@@ -1,4 +1,15 @@
 import { onLogin } from "../../ui/auth/login";
 import { LOGIN_FORM } from "../../api/constants.js";
 
-LOGIN_FORM.addEventListener("submit", onLogin);
+/**
+ * Listens for the login form submission event and calls the onLogin function.
+ * @function listenForLoginFormSubmission
+ * @returns {void}
+ * @example
+ * listenForLoginFormSubmission();
+ */
+function listenForLoginFormSubmission() {
+  LOGIN_FORM.addEventListener("submit", onLogin);
+}
+
+listenForLoginFormSubmission();
