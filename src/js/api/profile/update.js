02d735db5e1b9bd55db5a0686a9e3231a3b8113e
Leanne <lemeyer.no@gmail.com>
2024-09-28 10:45:40 +0200

    FIX: Removed original code and added mine
    Removed code that was already in file
    
    Added close update profile form function and display update profile form function
    
    Added event listeners for update profile button and cancel button
    
    Added constants for use in these functions

--- src/js/api/profile/update.js
+++ src/js/api/profile/update.js
@@ -1,1 +1,19 @@
-export async function updateProfile(username, { avatar, banner }) {}
+const UPDATE_PROFILE_BUTTON = document.getElementById("update-profile-button");
+const PROFILE_FORM = document.getElementById("Update-Profile");
+const CANCEL_BUTTON = document.getElementById("cancel-button");
+PROFILE_FORM.style.display = "none";
+
+export function closeUpdateProfileForm() {
+  if (PROFILE_FORM && PROFILE_FORM.style.display === "block") {
+    PROFILE_FORM.style.display = "none";
+  }
+}
+
+export function displayUpdateProfileForm() {
+  if (PROFILE_FORM && PROFILE_FORM.style.display === "none") {
+    PROFILE_FORM.style.display = "block";
+  }
+}
+
+UPDATE_PROFILE_BUTTON.addEventListener("click", displayUpdateProfileForm);
+CANCEL_BUTTON.addEventListener("click", closeUpdateProfileForm);
