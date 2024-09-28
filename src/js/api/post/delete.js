8ffcf963486e5e2ad8d56d6dd07ae37891017db9
Leanne <lemeyer.no@gmail.com>
2024-09-28 12:43:06 +0200

    Feat:  Added JS Docs

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -2,6 +2,28 @@ import { GET_POST_API } from "../constants";
 import { handleError } from "./create.js";
 import { headers } from "../headers.js";
 
+/**
+ * @async
+ * @function removePostFromAPI
+ * @param {string} postId
+ *
+ * @returns {Promise<void>} A promise that resolves
+ * when the post is deleted or rejects the request if
+ * an error occurs.
+ *
+ * @throws Will throw an error if the fetch request fails.
+ *
+ *
+ * @example
+ * // Example of how to call the removePostFromAPI function
+ * import { removePostFromAPI }
+ * from "./path/to/api/post/delete.js";
+ *
+ * const POST_ID = "123";
+ * removePostFromAPI(POST_ID);
+ *
+ */
+
 export async function removePostFromAPI(postId) {
   try {
     const RESPONSE = await fetch(`${GET_POST_API}${postId}`, {
