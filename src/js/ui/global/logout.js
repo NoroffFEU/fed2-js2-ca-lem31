95b09b4ceae300fd8cdd0e8a7e0e00180cd615de
Leanne <lemeyer.no@gmail.com>
2024-09-28 18:41:10 +0200

    Feat: JS DOCS
    Added JS DOCS for logout function

--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,5 +1,13 @@
 import { removeTokenFromStorage } from "../auth/logout.js";
 
+/**
+ * This function adds an event Listener to the logout button
+ * and calls the removeTokenFromStorage function.
+ * @function logout
+ * @returns {void}
+ * @example
+ * logout();
+ */
 export function logout() {
   const LOGOUT_BUTTON = document.getElementById("logout-button");
 
