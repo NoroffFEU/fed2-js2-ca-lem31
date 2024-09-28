381772b7cda382d2d80a673b716246561e9b764d
Leanne <lemeyer.no@gmail.com>
2024-09-28 18:47:42 +0200

    Feat: JS DOCS
    Added JS DOCS for retrievePostID function

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,5 +1,13 @@
 import { getSinglePost } from "../../api/post/read.js";
 
+/**
+ * This function retrieves a post ID from the URL and calls the getSinglePost function.
+ * @function retrievePostID
+ * @returns {void}
+ * @example
+ * retrievePostID();
+ *
+ */
 export function retrievePostID() {
   const PARAMS = new URLSearchParams(window.location.search);
   const POST_ID = PARAMS.get("id");
